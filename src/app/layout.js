363ba0a90c41e57b743bbc0a3ba30f602e222a0e
Jay-Kalariya <jaykalariya1209@gmail.com>
2026-01-27 18:32:23 +0530

    upd

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-// import SmoothScroll from "@/animations/SmoothScroll";
+import SmoothScroll from "@/animations/SmoothScroll";
 
 const helveticaNeue = localFont({
   src: [
@@ -40,11 +40,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={helveticaNeue.variable}>
-        {/* <SmoothScroll> */}
+        <SmoothScroll>
         <Header />
         {children}
         <Footer />
-        {/* </SmoothScroll> */}
+        </SmoothScroll>
       </body>
     </html>
   );
