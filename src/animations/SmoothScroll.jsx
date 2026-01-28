754af2adffe441903b0b143c88ba24043eaf5c94
Jay-Kalariya <jaykalariya1209@gmail.com>
2026-01-28 12:54:52 +0530

    Header Customize and menu bar animations added

--- src/animations/SmoothScroll.jsx
+++ src/animations/SmoothScroll.jsx
@@ -14,15 +14,25 @@ export default function SmoothScroll({ children }) {
   return (
     <ReactLenis
       root
+      // options={{
+      //   lerp: 0.2,  
+      //   duration: 1.1,
+      //   smoothWheel: true,
+      //   wheelMultiplier: 1,
+      //   touchMultiplier: 2,
+      //   easing: (t) => t * (2 - t),
+      //   infinite: false,
+      // }}
       options={{
-        lerp: 0.2, 
+        lerp: 0.08,
         duration: 1.1,
         smoothWheel: true,
-        wheelMultiplier: 1,
+        wheelMultiplier: 0.9,
         touchMultiplier: 2,
-        easing: (t) => t * (2 - t),
+        easing: (t) => 1 - Math.pow(1 - t, 3), // smoother than default easeOut
         infinite: false,
       }}
+
     >
       <LenisSync />
       {children}
