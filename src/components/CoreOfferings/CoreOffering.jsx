6c88e60ae0e85b5856d8810365a04caacf6e61d4
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2026-01-31 11:49:01 +0530

    Comment out CoreOffering component code
    Comment out the CoreOffering component code for future reference.

--- src/components/CoreOfferings/CoreOffering.jsx
+++ src/components/CoreOfferings/CoreOffering.jsx
@@ -1,105 +1,105 @@
-"use client";
+// "use client";
 
-import { useEffect, useRef } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import "./CoreOffering.scss";
-import Image from "next/image";
-import Banner from "@/images/coreBanner.png";
-import ParagraphTextReveal from "@/animations/ParagraphTextReveal";
+// import { useEffect, useRef } from "react";
+// import gsap from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import "./CoreOffering.scss";
+// import Image from "next/image";
+// import Banner from "@/images/coreBanner.png";
+// import ParagraphTextReveal from "@/animations/ParagraphTextReveal";
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-const CORE_OFFERINGS = [
-  {
-    title: "Interior Designing",
+// const CORE_OFFERINGS = [
+//   {
+//     title: "Interior Designing",
 
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic ",
-  },
-  {
-    title: "Architecture & Planning",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic ",
-  },
-  {
-    title: "3D Visualization",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic ",
-  },
-];
+//     description:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic ",
+//   },
+//   {
+//     title: "Architecture & Planning",
+//     description:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic ",
+//   },
+//   {
+//     title: "3D Visualization",
+//     description:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic ",
+//   },
+// ];
 
-const AccordionScroll = () => {
-  const containerRef = useRef(null);
-  const regionsRef = useRef([]);
+// const AccordionScroll = () => {
+//   const containerRef = useRef(null);
+//   const regionsRef = useRef([]);
 
-  useEffect(() => {
-    const ctx = gsap.context(() => {
-      gsap.timeline({
-        scrollTrigger: {
-          trigger: containerRef.current,
-          start: "top top",
-          end: () => `+=${regionsRef.current.length * 1600}`,
-          // 900
-          // scrub: 0.6,
-          scrub: 5,
-          // 2
-          pin: true,
-          pinSpacing: true,
-          anticipatePin: 1,
+//   useEffect(() => {
+//     const ctx = gsap.context(() => {
+//       gsap.timeline({
+//         scrollTrigger: {
+//           trigger: containerRef.current,
+//           start: "top top",
+//           end: () => `+=${regionsRef.current.length * 1600}`,
+//           // 900
+//           // scrub: 0.6,
+//           scrub: 5,
+//           // 2
+//           pin: true,
+//           pinSpacing: true,
+//           anticipatePin: 1,
 
-        },
-      }).to(regionsRef.current, {
-        height: "auto",
-        opacity: 1,
-        duration: 1,
-        ease: "none",
-        stagger: {
-          each: 2,
-          delay: 0.5,
-          yoyo: true,
-          repeat: 1,
-        },
-      });
-    }, containerRef);
+//         },
+//       }).to(regionsRef.current, {
+//         height: "auto",
+//         opacity: 1,
+//         duration: 1,
+//         ease: "none",
+//         stagger: {
+//           each: 2,
+//           delay: 0.5,
+//           yoyo: true,
+//           repeat: 1,
+//         },
+//       });
+//     }, containerRef);
 
-    return () => ctx.revert();
-  }, []);
+//     return () => ctx.revert();
+//   }, []);
 
-  return (
-    <section className="accordion-scroll" ref={containerRef}>
-      <h2 className="accordion-scroll__heading"><ParagraphTextReveal>CORE OFFERINGS</ParagraphTextReveal></h2>
+//   return (
+//     <section className="accordion-scroll" ref={containerRef}>
+//       <h2 className="accordion-scroll__heading"><ParagraphTextReveal>CORE OFFERINGS</ParagraphTextReveal></h2>
 
-      <div className="accordion-scroll__box">
-        <div className="accordion-scroll__content">
-          {CORE_OFFERINGS.map((item, index) => (
-            <div className="accordion-scroll__item" key={index}>
-              <h3 className="accordion-scroll__title">{item.title}</h3>
+//       <div className="accordion-scroll__box">
+//         <div className="accordion-scroll__content">
+//           {CORE_OFFERINGS.map((item, index) => (
+//             <div className="accordion-scroll__item" key={index}>
+//               <h3 className="accordion-scroll__title">{item.title}</h3>
 
-              <div
-                className="accordion-scroll__region"
-                ref={(el) => (regionsRef.current[index] = el)}
-                role="region"
-              >
-                <div className="accordion-scroll__media">
-                  <Image src={Banner} alt={item.title} />
+//               <div
+//                 className="accordion-scroll__region"
+//                 ref={(el) => (regionsRef.current[index] = el)}
+//                 role="region"
+//               >
+//                 <div className="accordion-scroll__media">
+//                   <Image src={Banner} alt={item.title} />
 
-                  <div className="accordion-scroll__overlay">
-                    <p className="accordion-scroll__desc">
-                      {item.description}
-                    </p>
-                    <button className="accordion-scroll__btn">
-                      Learn more
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
-};
+//                   <div className="accordion-scroll__overlay">
+//                     <p className="accordion-scroll__desc">
+//                       {item.description}
+//                     </p>
+//                     <button className="accordion-scroll__btn">
+//                       Learn more
+//                     </button>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
 
-export default AccordionScroll;
+// export default AccordionScroll;
