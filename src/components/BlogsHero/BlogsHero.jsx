d7856681d42ef7d4aa23835b67514f530f2dfea9
Jay-Kalariya <jaykalariya1209@gmail.com>
2026-02-06 14:23:12 +0530

    ds

--- src/components/BlogsHero/BlogsHero.jsx
+++ src/components/BlogsHero/BlogsHero.jsx
@@ -11,35 +11,35 @@ const BlogsHero = () => {
 //   const { title, description, location, image } = project;
 
   return (
-    <section className="ProjectHero">
-      <div className="ProjectHero__wrapper">
+    <section className="blogsHero">
+      <div className="blogsHero__wrapper">
         {/* Background Image */}
-        <div className="ProjectHero__image">
+        <div className="blogsHero__image">
           <Image
             src={Banner}
             alt="none"
             fill
-            className="ProjectHero__img"
+            className="blogsHero__img"
           />
         </div>
 
      
-        <div className="ProjectHero__content">
-          <div className="ProjectHero__left">
-            <h1 className="ProjectHero__title">
+        <div className="blogsHero__content">
+          <div className="blogsHero__left">
+            <h1 className="blogsHero__title">
               BLOGS
             </h1>
           </div>
 
-          <div className="ProjectHero__right">
-            <p className="ProjectHero__desc">
+          <div className="blogsHero__right">
+            <p className="blogsHero__desc">
            Lorem Ipsum is simply dummy text of the printing and typesetting industry
             </p>
           </div>
         </div>
 
         {/* Location Badge */}
-        <div className="ProjectHero__location">
+        <div className="blogsHero__location">
           {/* <Image src={LocationSvg} alt="location" /> */}
           <span>ALL BLOGS</span>
         </div>
