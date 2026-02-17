a850928add58daad8f77dd8d200efb4760148c01
Jay-Kalariya <jaykalariya1209@gmail.com>
2026-02-17 17:21:26 +0530

    experience section animated --j

--- src/components/ExperienceSec/Experience.jsx
+++ src/components/ExperienceSec/Experience.jsx
@@ -1,24 +1,115 @@
 "use client";
 
-import React from "react";
+
+import { useEffect, useRef } from "react";
 import Image from "next/image";
 import "./Experience.scss";
 import ImgMain from "@/images/image1.png";
 import ImgSideTop from "@/images/image2.png";
 import ImgSideBottom from "@/images/image3.png";
 import ParagraphTextReveal from "@/animations/ParagraphTextReveal";
+import { motion } from "framer-motion";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const Experience = () => {
+  const imageRef = useRef(null);
+  const imageRef1 = useRef(null);
+  const sectionRef = useRef(null);
+  const imageRef2 = useRef(null);
+  useEffect(() => {
+    gsap.fromTo(
+      sectionRef.current,
+      { y: 0, scale: 1.2, opacity: 0, clipPath: "inset(100% 0% 0% 0%)" },
+      {
+        y: 0,
+        scale: 1,
+        opacity: 1,
+        clipPath: "inset(0% 0% 0% 0%)",
+        ease: "power4.out",
+        scrollTrigger: {
+          trigger: sectionRef.current,
+          start: "top 80%",
+          end: "bottom 40%",
+          scrub: 2, // Smoothness of the scroll animation
+        },
+      }
+    );
+  }, []);
+  // const eleganceContainer = useRef(null);
+  useEffect(() => {
+    gsap.fromTo(
+      imageRef.current,
+      { y: -100, scale: 1.7, opacity: 0, clipPath: "inset(100% 0% 0% 0%)" },
+      {
+        y: 0,
+        scale: 1,
+        opacity: 1,
+        clipPath: "inset(0% 0% 0% 0%)",
+        ease: "power4.out",
+        scrollTrigger: {
+          trigger: imageRef.current,
+          start: "top 70%",
+          end: "bottom 40%",
+          scrub: 2, // Smoothness of the scroll animation
+        },
+      }
+    );
+  }, []);
+  useEffect(() => {
+    gsap.fromTo(
+      imageRef1.current,
+      { y: -100, scale: 1.7, opacity: 0, clipPath: "inset(100% 0% 0% 0%)" },
+      {
+        y: 0,
+        scale: 1,
+        opacity: 1,
+        clipPath: "inset(0% 0% 0% 0%)",
+        ease: "power4.out",
+        scrollTrigger: {
+          trigger: imageRef1.current,
+          start: "top 90%",
+          end: "bottom 50%",
+          scrub: 2, // Smoothness of the scroll animation
+        },
+      }
+    );
+  }, []);
+  useEffect(() => {
+    gsap.fromTo(
+      imageRef2.current,
+      { y: -100, scale: 1.7, opacity: 0, clipPath: "inset(100% 0% 0% 0%)" },
+      {
+        y: 0,
+        scale: 1,
+        opacity: 1,
+        clipPath: "inset(0% 0% 0% 0%)",
+        ease: "power4.out",
+        scrollTrigger: {
+          trigger: imageRef2.current,
+          start: "top 70%",
+          end: "bottom 40%",
+          scrub: 2, // Smoothness of the scroll animation
+        },
+      }
+    );
+  }, []);
   return (
-    <section className="experience">
+    <section className="experience" ref={sectionRef}>
       <div className="experience__container">
         {/* Left Content */}
         <div className="experience__content">
           <span className="experience__count">20</span>
-          <h2 className="experience__title">
-            <ParagraphTextReveal>
-              WHERE ELEGANCE <br /> MEETS DESIRE</ParagraphTextReveal>
-          </h2>
+          <motion.h2
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1.5, delay: 0.5 }}
+            viewport={{ once: true }}
+            className="experience__title">
+
+            WHERE ELEGANCE <br /> MEETS DESIRE
+          </motion.h2>
 
           <div className="experience__divider">
             <span>YEARS OF EXPERIENCE</span>
@@ -35,17 +126,17 @@ const Experience = () => {
         </div>
 
         {/* Right Images */}
-        <div className="experience__images">
-          <div className="experience__image experience__image--main">
-            <Image src={ImgMain} alt="Interior Design"  />
+        <div className="experience__images" ref={imageRef}>
+          <div className="experience__image experience__image--main" ref={imageRef1}>
+            <Image src={ImgMain} alt="Interior Design" />
           </div>
 
-          <div className="experience__image experience__image--top">
-            <Image src={ImgSideTop} alt="Luxury Space"  />
+          <div className="experience__image experience__image--top" ref={imageRef2}>
+            <Image src={ImgSideTop} alt="Luxury Space" />
           </div>
 
           <div className="experience__image experience__image--bottom">
-            <Image src={ImgSideBottom} alt="Elegant Interior"  />
+            <Image src={ImgSideBottom} alt="Elegant Interior" />
           </div>
         </div>
       </div>
