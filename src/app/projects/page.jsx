1b46cb3ccdb8338efde80788359591338395f11e
Jay-Kalariya <jaykalariya1209@gmail.com>
2026-02-24 18:21:34 +0530

    Project detail page completed --j

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,21 +1,69 @@
-"use client";
-
 import ProjectBanner from "@/components/ProjectBanner/ProjectBanner";
 import ProjectDetail from "@/components/ProjectDetails/ProjectDetail";
-import { useEffect } from "react";
 
-const ProjectsPage = () => {
-  useEffect(() => {
-    document.title = "Projects | Unity Interior";
-  }, []);
+async function getData() {
+  const [projectsRes, categoriesRes] = await Promise.all([
+    fetch(
+      "https://unityinteriorsadmin.humbeestudio.xyz/wp-json/wp/v2/projects?_embed&acf_format=standard",
+      { next: { revalidate: 60 } }
+    ),
+    fetch(
+      "https://unityinteriorsadmin.humbeestudio.xyz/wp-json/wp/v2/project-category",
+      { next: { revalidate: 60 } }
+    ),
+  ]);
 
-  return (
-    <div>
-      <ProjectBanner />
-      <ProjectDetail />
-    </div>
-  )
+  if (!projectsRes.ok || !categoriesRes.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
+  const projects = await projectsRes.json();
+  const categories = await categoriesRes.json();
+
+  return { projects, categories };
 }
 
-export default ProjectsPage;
+export default async function ProjectsPage() {
+  const { projects, categories } = await getData();
+
+  return (
+    <>
+      <ProjectBanner />
+      <ProjectDetail projects={projects} categories={categories} />
+    </>
+  );
+}
+
+
+
+// import ProjectBanner from "@/components/ProjectBanner/ProjectBanner";
+// import ProjectDetail from "@/components/ProjectDetails/ProjectDetail";
+// import { useEffect } from "react";
+// async function getProjects() {
+//   const res = await fetch(
+//     "https://unityinteriorsadmin.humbeestudio.xyz/wp-json/wp/v2/projects?_embed",
+//     {
+//       next: { revalidate: 60 }, // ISR
+//     }
+//   );
+
+//   if (!res.ok) throw new Error("Failed to fetch projects");
+//   return res.json();
+// }
+
+// export default async function ProjectsPage() {
+//   const projects = await getProjects();
+//   useEffect(() => {
+//     document.title = "Projects | Unity Interior";
+//   }, []);
+
+//   return (
+//     <div>
+//       <ProjectBanner />
+//         <ProjectDetail projects={projects} />
+//     </div>
+//   )
+
+// }
+
+// // export default ProjectsPage;
